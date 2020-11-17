37403f3c0480b19b24d3458cb5081dbb1bead9a6
buenazo <74580416+buenazo@users.noreply.github.com>
2020-11-17 11:39:06 -0500

    Update Movimiento.cpp

--- Movimiento.cpp
+++ Movimiento.cpp
@@ -38,7 +38,7 @@ void Movimiento::Run(Bitmap^ bmp_monigote, Graphics^ g, Bitmap^ bmp_mapa)
 	for (int i = 0; i < 50; i++)
 	{
 
-		if (Colision(obj_monigote, mapa[i]) && (i == 1 || i == 2 || i == 3 || i == 4 || i == 11 || i == 21 || i == 22 || i == 24 || i == 31 || i == 32 || i == 34 || i == 44))
+		if (Colision(obj_monigote, mapa[i]) && (mapa[i]->retornar_limite()))
 		{
 			obj_monigote->cambiar_X(obj_monigote->retornar_X() - obj_monigote->retornar_DX());
 			obj_monigote->cambiar_Y(obj_monigote->retornar_Y() - obj_monigote->retornar_DY());
@@ -66,56 +66,56 @@ Movimiento::~Movimiento() {
 	
 }
 void Movimiento::Dibujar_Mapa(Graphics^ g, Bitmap^ bmp_mapa) {
-	mapa[0]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[1]->Dibujar_Imagen(g, bmp_mapa, 0, 7);
-	mapa[2]->Dibujar_Imagen(g, bmp_mapa, 2, 8);
-	mapa[3]->Dibujar_Imagen(g, bmp_mapa, 2, 8);
-	mapa[4]->Dibujar_Imagen(g, bmp_mapa, 1, 9);
-	mapa[5]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[6]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[7]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[8]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[9]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[10]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[11]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[12]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[13]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[14]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[15]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[16]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[17]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[18]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[19]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[20]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[21]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[22]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[23]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[24]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[25]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[26]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[27]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[28]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[29]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[30]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[31]->Dibujar_Imagen(g, bmp_mapa, 2, 7);
-	mapa[32]->Dibujar_Imagen(g, bmp_mapa, 2, 9);
-	mapa[33]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[34]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[35]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[36]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[37]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[38]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[39]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[40]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[41]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[42]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[43]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[44]->Dibujar_Imagen(g, bmp_mapa, 1, 7);
-	mapa[45]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[46]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[47]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[48]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
-	mapa[49]->Dibujar_Imagen(g, bmp_mapa, 0, 0);
+	mapa[0]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[1]->Dibujar_Imagen(g, bmp_mapa, 0, 7, true);
+	mapa[2]->Dibujar_Imagen(g, bmp_mapa, 2, 8, true);
+	mapa[3]->Dibujar_Imagen(g, bmp_mapa, 2, 8, true);
+	mapa[4]->Dibujar_Imagen(g, bmp_mapa, 1, 9, true);
+	mapa[5]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[6]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[7]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[8]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[9]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[10]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[11]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[12]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[13]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[14]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[15]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[16]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[17]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[18]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[19]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[20]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[21]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[22]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[23]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[24]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[25]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[26]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[27]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[28]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[29]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[30]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[31]->Dibujar_Imagen(g, bmp_mapa, 2, 7, true);
+	mapa[32]->Dibujar_Imagen(g, bmp_mapa, 2, 9, true);
+	mapa[33]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[34]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[35]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[36]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[37]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[38]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[39]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[40]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[41]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[42]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[43]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[44]->Dibujar_Imagen(g, bmp_mapa, 1, 7, true);
+	mapa[45]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[46]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[47]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[48]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
+	mapa[49]->Dibujar_Imagen(g, bmp_mapa, 0, 0, false);
 }
 bool Movimiento::Colision(Punto* obj1, Punto* obj2) {
 	Rectangle rect1 = Rectangle(obj1->retornar_X(),
@@ -128,3 +128,4 @@ bool Movimiento::Colision(Punto* obj1, Punto* obj2) {
 		obj2->retornar_Alto()/2);
 	return rect1.IntersectsWith(rect2);
 }
+//System::DateTime::Now.Ticks : srand para Random() -> Random(System::DateTime::Now.Ticks)
