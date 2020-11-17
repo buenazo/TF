cd7863ddbcfcf627ee843818c0bcff773529453b
buenazo <74580416+buenazo@users.noreply.github.com>
2020-11-17 11:37:28 -0500

    Update Mapa.cpp

--- Mapa.cpp
+++ Mapa.cpp
@@ -6,15 +6,18 @@ Mapa::Mapa(int px, int py, Bitmap^ bmp) : Punto(px, py) {
 	dx = dy = 0;
 	f = 0;
 	c = 0;
+	limite = false;
 }
-void Mapa::Dibujar_Imagen(Graphics^ g, Bitmap^ bmp, int pf, int pc)
+void Mapa::Dibujar_Imagen(Graphics^ g, Bitmap^ bmp, int pf, int pc, bool plimite)
 {
 	f = pf;
 	c = pc;
+	limite = plimite;
 	Rectangle porcion = Rectangle(c * w, f * h, w, h);
 	Rectangle zoom = Rectangle(x, y, w, h);
 	g->DrawImage(bmp, zoom, porcion, GraphicsUnit::Pixel);
 }
 float Mapa::retornar_Ancho() { return w; }
 float Mapa::retornar_Alto() { return h; }
+bool Mapa::retornar_limite() { return limite; }
 Mapa::~Mapa() {}
